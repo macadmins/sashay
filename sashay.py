40181cb51a84aa0b8a1608372bba70b3f43bbc94
Allister Banks <arubdesu@me.com>
2015-09-27 15:12:43 -0400

    Make 'devmode' for easier debug, continue linting
    add ‘devmode’ and ‘logs’ options so it’ll skip final message send and
    sanity checks for local dev (without having to manually override in
    script). Make sanities its own function so those variables are
    contained/skipped all at once. Continue linting to get more PEP8-ified.
    
    Still only server4.1+ compliant, starting work towards 5

--- sashay.py
+++ sashay.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+"""Parses Server.app's Caching Servers Debug Log, Sends Reports
+   See README and LICENSE for more information. Originally created
+   by Erik Gomez, pythonified by Allister Banks, 2015"""
+
+
 import bz2
 import collections
 import datetime
@@ -11,29 +16,30 @@ import sys
 import tempfile
 from CoreFoundation import CFPreferencesCopyAppValue
 
-#sanity checking - since I'm going through the trouble of pure python bunzip'ing
-plist_path = '/Applications/Server.app/Contents/Info.plist'
-server_app_version = CFPreferencesCopyAppValue('CFBundleShortVersionString', plist_path)
-if not server_app_version:
-    print "Can't find Server.app, are you running this on your Mac server instance?"
-    sys.exit(2)
-elif server_app_version < 4.1:
-    print "Not Version 4.1(+) of Server.app"
-    sys.exit(3)
-else:
-    PREF_PATH = '/Library/Server/Caching/Config/Config.plist'
-    try:
-        LOGIDENTITYPREF = CFPreferencesCopyAppValue('LogClientIdentity', PREF_PATH)
-        if not LOGIDENTITYPREF == 'true':
-            print """This will be a very spare/boring report if you don't run this command:
-                  sudo serveradmin settings caching:LogClientIdentity = 1"""
-    except Exception as e:
-        raise e
 
+def sanities():
+    """preflight checks to ensure compat., skipped if run in dev mode"""
+    plist_path = '/Applications/Server.app/Contents/Info.plist'
+    server_app_version = CFPreferencesCopyAppValue('CFBundleShortVersionString', plist_path)
+    if not server_app_version:
+        print "Can't find Server.app, are you running this on your Mac server instance?"
+        sys.exit(2)
+    elif server_app_version < 4.1:
+        print "Not Version 4.1(+) of Server.app"
+        sys.exit(3)
+    else:
+        pref_path = '/Library/Server/Caching/Config/Config.plist'
+        try:
+            logidentitypref = CFPreferencesCopyAppValue('LogClientIdentity', pref_path)
+            if not logidentitypref == 'true':
+                print """This will be a very spare/boring report if you don't run this command:
+                      sudo serveradmin settings caching:LogClientIdentity = 1"""
+        except Exception as e:
+            raise e
+    if os.geteuid() != 0:
+        exit("For the final message send(only), this(currently) needs to be run with 'sudo'.")
 
-if os.geteuid() != 0:
-    exit("For the final message send(only), this(currently) needs to be run with 'sudo'.")
-
+# utility methods
 def normalize_gbs(mb_or_gb, val_to_operate_on):
     """Used when calculating bandwidth. Takes an index to check, and if MB,
        returns the applicable index divided by 1024 to normalize on GBs"""
@@ -44,9 +50,11 @@ def normalize_gbs(mb_or_gb, val_to_operate_on):
     else:
         return float(val_to_operate_on)
 
+
 def alice(list_to_get_extremes):
     """One pill makes you taller... calculates bandwidth used delta"""
     return max(list_to_get_extremes) - min(list_to_get_extremes)
+
 
 def gen_mb_or_gb(floaty):
     """Shows bandwidth in MBs if less than 1GB. Returns string"""
@@ -62,11 +70,13 @@ def get_start(from_datetime):
     delta_object = datetime.timedelta(days=int(from_datetime))
     start_datetime = str(datetime.datetime.today() - delta_object)[:-3] # lops off UTC's millisecs
     return start_datetime
+
+
 def join_bzipped_logs(dir_of_logs):
     """Creates tempfile, wildcard searches for all archive logs, then uses bz2 module
        to unpack and append them all to the tempfile. Returns file(list of strings) once populated"""
-    #setup tempfile 
-    unbzipped_logs = tempfile.mkstemp(suffix='.log',prefix='sashayTemp-')[1]
+    #setup tempfile
+    unbzipped_logs = tempfile.mkstemp(suffix='.log', prefix='sashayTemp-')[1]
     das_bzips = "".join([dir_of_logs, '/Debug-*'])
     bunch_of_bzips = glob.glob(das_bzips)
     opened_masterlog = open(unbzipped_logs, 'w')
@@ -82,12 +92,13 @@ def join_bzipped_logs(dir_of_logs):
     opened_masterlog.close()
     return unbzipped_logs
 
-def separate_out_range_and_build_lists(dir_of_logs, unbzipped_logs, start_datetime, to_datetime):
+
+def separate_range_build_list(dir_of_logs, unbzipped_logs, start_datetime, to_datetime):
     # todo - this is a bit big and nasty, but good enough for now
     """Opens current debug.log and un-bz'd logs and builds new list of loglines
        that fall within our reporting period. Returns two lists of strings: one for bandwidth,
        other for eventual parsing of filetypes served, device os/model, and ips devices accessed from."""
-    our_range_logline_str_list,bandwidth_lines_list,filetype_lines_list,service_restart_timestamps=[],[],[],[]
+    our_range_logline_str_list, bandwidth_lines_list, filetype_lines_list, service_restart_timestamps = [], [], [], []
     try:
         with open(os.path.join(dir_of_logs, 'Debug.log'), 'rU') as current, open(unbzipped_logs, 'rU') as unzipped:
             for f in current, unzipped:
@@ -128,10 +139,11 @@ def separate_out_range_and_build_lists(dir_of_logs, unbzipped_logs, start_dateti
                         filetype_lines_list.append(logline_str.split())
     return bandwidth_lines_list, filetype_lines_list, more_recent_svc_hup, new_start_datetime
 
+
 def parse_bandwidth(bandwidth_lines_list):
     """ Use indexed fields in log lines to build list of bandwidth transferred, normalizes in GBs,
         then parses deltas for data served from cache or streamed from apple/peers. Returns strings"""
-    logged_bytes_from_cache,logged_bytes_from_apple,logged_bytes_from_peers=[],[],[]
+    logged_bytes_from_cache, logged_bytes_from_apple, logged_bytes_from_peers = [], [], []
     for each in bandwidth_lines_list:
         strip_parens = (each[15])[1:] # silly log line cleanup
         logged_bytes_from_cache.append(normalize_gbs(each[6], each[5]))
@@ -140,15 +152,15 @@ def parse_bandwidth(bandwidth_lines_list):
             logged_bytes_from_peers.append(normalize_gbs(each[20], each[19]))
     daily_total_from_cache = alice(logged_bytes_from_cache)
     daily_total_from_apple = alice(logged_bytes_from_apple)
-    # check for peers
+    # check for peers, set default
+    peer_amount = 'no peer servers detected'
     if len(logged_bytes_from_peers) > 1:
         if max(logged_bytes_from_peers) > 0.1:
             daily_total_from_peers = alice(logged_bytes_from_peers)
             daily_total_from_apple = daily_total_from_apple - daily_total_from_peers
             peer_amount = 'along with %s from peers' % gen_mb_or_gb(daily_total_from_peers)
-    else:
-        peer_amount = 'no peer servers detected'
     return daily_total_from_cache, daily_total_from_apple, peer_amount
+
 
 def get_device_stats(filetype_lines_list):
     """Parses out device stats, returns list motherlode"""
@@ -156,28 +168,27 @@ def get_device_stats(filetype_lines_list):
 # ['2015-06-30', '12:31:04.095', '#eLTtl5KfMlrA', 'Request', 'from', '172.20.202.245:61917', '[itunesstored/1.0', 'iOS/8.3', 'model/iPhone7,1', 'build/12F70', '(6;', 'dt:107)]', 'for', 'http://a1254.phobos.apple.com/us/r1000/038/Purple7/v4/23/23/5e/23235e5d-1a12-f381-c001-60acfe6a56ff/zrh1611131113630130772.D2.pd.ipa']
 # ['2015-06-30', '12:32:19.554', '#6d3LgXpVcHAU', 'Request', 'from', '172.18.20.102:52880', '[Software%20Update', '(unknown', 'version)', 'CFNetwork/720.3.13', 'Darwin/14.3.0', '(x86_64)]', 'for', 'http://swcdn.apple.com/content/downloads/58/34/031-25780/u1bqpe4ggzdp86utj2esnxfj4xq5izwwri/FirmwareUpdate.pkg']
 # ['2015-06-30', '14:09:00.230', '#sNn+egdFxN7m', 'Request', 'from', '172.18.81.204:60025', '[Software%20Update', '(unknown', 'version)', 'CFNetwork/596.6.3', 'Darwin/12.5.0', '(x86_64)', '(MacBookAir6%2C2)]', 'for', 'http://swcdn.apple.com/content/downloads/15/59/031-21808/qylh17vrdgnipjibo2avj3nbw8y2pzeito/Safari6.2.7MountainLion.pkg']
-    IPLog,OSLog,ModelLog,ipas,epubs,pkgs,zips=[],[],[],[],[],[],[]
+    iplog, oslog, modellog, ipas, epubs, pkgs, zips = [], [], [], [], [], [], []
     for filelog in filetype_lines_list:
         if filelog[5].startswith('172') or filelog[5].startswith('192') or filelog[5].startswith('10.'):
             strip_port = (filelog[5])[:-6]
-            IPLog.append(strip_port)
-            if filelog[10].startswith('Darwin/12'):#User agent-based Mac OS detection
-                OSLog.append('Mac OS 10.8.x')
-            elif filelog[10].startswith('Darwin/13'):
-                OSLog.append('Mac OS 10.9.x')
-            elif filelog[10].startswith('Darwin/14'):
-                OSLog.append('Mac OS 10.10.x')
+            iplog.append(strip_port)
+            if not filelog[10].startswith('Darwin'):
+                if filelog[7] == '(unknown':
+                    modellog.append('Unknown Mac')
+                else:
+                    oslog.append(filelog[7])#Whereas iOS just logs without a fuss
             else:
-                OSLog.append(filelog[7])#Whereas iOS just logs without a fuss
+                osver_dict = {'12':'Mac OS 10.8.x', '13':'Mac OS 10.9.x', '14':'Mac OS 10.10.x', '15':'Mac OS 10.11.x'}
+                os_ver = (filelog[10])[7:9] #User agent-based Mac OS detection
+                oslog.append(osver_dict.get(os_ver))
             if len(filelog) == 15:# For some reason you only seem to get Mac models when logline has 15 sections
                 if filelog[12].startswith('dt:'):
-                    ModelLog.append((filelog[8])[6:])
+                    modellog.append((filelog[8])[6:])
                 else:
-                    ModelLog.append(filelog[12])
-            elif filelog[7] == '(unknown':
-                ModelLog.append('Unknown Mac')
+                    modellog.append(filelog[12])
             else:
-                ModelLog.append((filelog[8])[6:])#...whereas iOS just logs without a fuss(noticing a pattern?)
+                modellog.append((filelog[8])[6:])#...whereas iOS just logs without a fuss(noticing a pattern?)
             if (filelog[-1]).endswith('ipa'):
                 ipas.append(filelog[-1])
             elif (filelog[-1]).endswith('epub'):
@@ -186,7 +197,8 @@ def get_device_stats(filetype_lines_list):
                 pkgs.append(filelog[-1])
             elif (filelog[-1]).endswith('zip'):
                 zips.append(filelog[-1])
-    return IPLog,OSLog,ModelLog,ipas,epubs,pkgs,zips
+    return iplog, oslog, modellog, ipas, epubs, pkgs, zips
+
 
 def parse_prods(prodlist, name):
     """Returns a string with prod reporting format, or empty string if no products found"""
@@ -196,6 +208,7 @@ def parse_prods(prodlist, name):
         return '%d different %s were requested %d times, \n' % (individ_prods, name, sum_of_prods)
     else:
         return ''
+
 
 def report_rounder(key_total):
     """takes the total number of 'keyed' items and returns an int to round displayed results to"""
@@ -236,47 +249,57 @@ def main():
                  help="""Report on total/unique zips (assuming for iOS firmware).""")
     p.add_option('--subject', '-s', dest='subject_prefix', default='Daily',
                  help="""Adds prefix to message, e.g. for denoting arbitrary date ranges, 'daily/weekly/monthly'.""")
+    p.add_option('--dev', '-d', dest='devmode', default=False,
+                 help="""Skips sanity checks for dev work, remember to override dir_of_logs""")
+    p.add_option('--logs', '-l', dest='logdir', default='/Library/Server/Caching/Logs',
+                 help="""Dir of caching server debug logs, default = /Library/Server/Caching/Logs""")
 
     options, arguments = p.parse_args()
-    dir_of_logs = '/Library/Server/Caching/Logs'                        #debug
+    # get to work
+    if not options.devmode:
+        sanities()
+    dir_of_logs = options.logdir
     start_datetime = get_start(options.from_datetime)
     unbzipped_logs = join_bzipped_logs(dir_of_logs)
-    (bandwidth_lines_list, filetype_lines_list, more_recent_svc_hup, new_start_datetime) = separate_out_range_and_build_lists(dir_of_logs, unbzipped_logs, start_datetime, options.to_datetime)
+    (bandwidth_lines_list, filetype_lines_list, more_recent_svc_hup, new_start_datetime) = separate_range_build_list(dir_of_logs, unbzipped_logs, start_datetime, options.to_datetime)
+    if options.devmode:
+        print '-' * 14, 'DEBUG', '-' * 14, '\nAll Args:', sys.argv[1:]
+        print 'New(?) start_datetime:', new_start_datetime, '\n'
     (daily_total_from_cache, daily_total_from_apple, peer_amount) = parse_bandwidth(bandwidth_lines_list)
-    (IPLog,OSLog,ModelLog,ipas,epubs,pkgs,zips) = get_device_stats(filetype_lines_list)
+    (iplog, oslog, modellog, ipas, epubs, pkgs, zips) = get_device_stats(filetype_lines_list)
 
     #build message
     message = ["Download requests served from cache:", gen_mb_or_gb(daily_total_from_cache), '\n',
-        "Amount streamed from Apple (",peer_amount,"):" , gen_mb_or_gb(daily_total_from_apple), '\n',
-        "(Potential) Net bandwidth saved:",
-        gen_mb_or_gb(daily_total_from_cache - daily_total_from_apple), '\n', ""]
+               "Amount streamed from Apple (" + peer_amount + "):", gen_mb_or_gb(daily_total_from_apple), '\n',
+               "(Potential) Net bandwidth saved:",
+               gen_mb_or_gb(daily_total_from_cache - daily_total_from_apple), '\n', ""]
     if more_recent_svc_hup:
         disclaimer1 = ['\n', "  * NOTE: Stats are only displayed from the last caching service restart,", new_start_datetime, '\n']
         message += disclaimer1
     if options.modelvers:
-        sum_of_models = len(ModelLog)
-        individs = len(set(ModelLog))
+        sum_of_models = len(modellog)
+        individs = len(set(modellog))
         key_total1 = report_rounder(individs)
-        counter=collections.Counter(ModelLog)
+        counter = collections.Counter(modellog)
         model_tally = ['\n', 'The', str(key_total1), 'most frequently seen types of devices (of', str(individs), 'unique devices in total, followed by their count) were:', '\n\t', str(counter.most_common(key_total1)),
-            '\n', 'The server was accessed by those devices', str(sum_of_models), 'times,']
-        if 'Unknown Mac' in ModelLog:
+                       '\n', 'The server was accessed by those devices', str(sum_of_models), 'times,']
+        if 'Unknown Mac' in modellog:
             model_tally.append('(Unique devices above do not include some unspecified Macs)')
         message += model_tally
     if options.network_ips:
         subnet_list = []
-        for addy in IPLog:
+        for addy in iplog:
             just_sub = re.search('(\d{1,3}\.){2}\d{1,3}', addy)
             subnet_list.append(just_sub.group())
         individ_subs = len(set(subnet_list))
         key_total2 = report_rounder(individ_subs)
-        counter=collections.Counter(subnet_list)
+        counter = collections.Counter(subnet_list)
         ip_tally = ['\n', "IP ranges that devices most frequently accessed this server from were:", '\n\t', str(counter.most_common(key_total2)), '\n']
         message += ip_tally
     if options.os_revisions:
-        individ_osen = len(set(OSLog))
+        individ_osen = len(set(oslog))
         key_total3 = report_rounder(individ_osen)
-        counter=collections.Counter(OSLog)
+        counter = collections.Counter(oslog)
         os_tally = ['\n', 'Of the', str(individ_osen), 'different OS versions seen across all devices, the', str(key_total3), 'most frequent were:', '\n\t', str(counter.most_common(key_total3)), '\n\n']
         message += os_tally
     final_filetypes = []
@@ -291,12 +314,13 @@ def main():
     message += final_filetypes
 
     if options.subject_prefix:
-        message.insert(0, options.subject_prefix) 
+        message.insert(0, options.subject_prefix)
 
-    print(' '.join(message))                                                        #debug
+    print ' '.join(message)                                                        #debug
     final_msg_list = ['/Applications/Server.app/Contents/ServerRoot/usr/sbin/server postAlert CustomAlert Common subject "', options.subject_prefix, 'Caching Server Data:', new_start_datetime[:-4], 'through', options.to_datetime[:-4] + '"', 'message', '"' + ' '.join(message) + '"', '<<<""']
     final_message = ' '.join(final_msg_list)
-    subprocess.call(final_message, shell=True)
+    if not options.devmode:
+        subprocess.call(final_message, shell=True)
 
 if __name__ == '__main__':
     main()
